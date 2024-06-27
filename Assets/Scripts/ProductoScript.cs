54af817cad3749e58282294802bcda6eb9486362
sositaivan <ivoanimator@gmail.com>
2024-06-27 14:50:57 -0300

    Inicio spawner

--- Assets/Scripts/ProductoScript.cs
+++ Assets/Scripts/ProductoScript.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class ProductoScript : MonoBehaviour
 {
+    public GameObject[] productoAsset;
+    public GameObject[] productoSpawn;
+    public float spawnDelay = 1.5f;
+    public float screenChangeDelay = 5.5f;
+    public int cantidad;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cantidad = Random.Range(4, 20);
+        int elemento = Random.Range(0, 20);
+        q1|
     }
 
     // Update is called once per frame
