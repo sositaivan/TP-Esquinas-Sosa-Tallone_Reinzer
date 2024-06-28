8820979b5e5efdb0f1e935a00c95cac4bde451af
NicolasEsquinas <esquinasnico@gmail.com>
2024-06-27 22:57:54 -0300

    Sript input field Sosa Esquinas

--- Assets/Scripts/ProductoScript.cs
+++ Assets/Scripts/ProductoScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ProductoScript : MonoBehaviour
@@ -11,6 +12,10 @@ public class ProductoScript : MonoBehaviour
     public SpawnerPosition randomSpawn;
     public int objectCounter = 0;
     public int maxSpawnedObjects;
+    public InputField CantidadIngresada;
+    public Text Txt_correcto;
+    public Text Txt_incorrect;
+    
     void Start()
     {
         InvokeRepeating(nameof(FoodSpawner), 0, interval);
@@ -42,5 +47,17 @@ public class ProductoScript : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void BotonResponder()
+    {
+        int NumeroIngresado = int.Parse(CantidadIngresada.text);
+        if(NumeroIngresado == objectCounter)
+        {
+            Txt_correcto.gameObject.SetActive(true);
+        }
+        else
+        {
+            Txt_incorrect.gameObject.SetActive(true);
+        }
     }
 }
